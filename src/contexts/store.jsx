71aac45e642c1e937544f78ab0037cb8532c4ecf
Arulmozhikumar <arulmozhikumar7@gmail.com>
2024-05-24 20:44:36 +0530

    feat: add 'birthday' variable to state management in store.jsx

--- src/contexts/store.jsx
+++ src/contexts/store.jsx
@@ -1,6 +1,8 @@
 import { create } from "zustand";
 
-export const useStore = create((set) => ({
+export const useUser = create((set) => ({
   user: null,
   setUser: (user) => set({ user }),
+  birthdays: [],
+  setBirthdays: (birthdays) => set({ birthdays: birthdays }),
 }));
