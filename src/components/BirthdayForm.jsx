d0ba4b0bebd7d7e95c234bd4199d28877cbb8809
Arulmozhikumar <arulmozhikumar7@gmail.com>
2024-05-25 15:34:17 +0530

    refactor: utilized the form for both add and edit functions

--- src/components/BirthdayForm.jsx
+++ src/components/BirthdayForm.jsx
@@ -1,39 +1,58 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { useUser } from "../contexts/store";
 import { storage } from "../Auth/firebase";
 import firestore from "../Auth/firestore";
 import { getBirthdays } from "../hooks/useGetBirthdays";
 import toast from "react-hot-toast";
-const BirthdayForm = () => {
-  const { user, setFormModal, setBirthdays } = useUser();
-  const [name, setName] = useState("");
-  const [date, setDate] = useState("");
+
+const BirthdayForm = ({ initialData = {}, onClose }) => {
+  const { user, setBirthdays } = useUser();
+  const [name, setName] = useState(initialData.name || "");
+  const [date, setDate] = useState(initialData.date || "");
   const [imageFile, setImageFile] = useState(null);
+  const [imageUrl, setImageUrl] = useState(initialData.imageUrl || "");
   const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (initialData.imageUrl) {
+      setImageUrl(initialData.imageUrl);
+    }
+  }, [initialData]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     setIsLoading(true);
     try {
-      let imageUrl = "";
       if (imageFile) {
         const imageRef = ref(storage, `images/${imageFile.name}`);
         await uploadBytes(imageRef, imageFile);
-        imageUrl = await getDownloadURL(imageRef);
+        const url = await getDownloadURL(imageRef);
+        setImageUrl(url);
       }
-      await firestore.addBirthday(user, name, date, imageUrl);
-      toast.success("Birthday added successfully");
-      setIsLoading(false);
-      setFormModal(false);
+
+      if (initialData.id) {
+        await firestore.editBirthday(
+          user,
+          initialData.id,
+          name,
+          date,
+          imageUrl
+        );
+        toast.success("Birthday edited successfully");
+      } else {
+        await firestore.addBirthday(user, name, date, imageUrl);
+        toast.success("Birthday added successfully");
+      }
+
       setBirthdays(await getBirthdays(user.uid));
-      setName("");
-      setDate("");
-      setImageFile(null);
+      setIsLoading(false);
+      onClose();
     } catch (error) {
-      console.error("Error adding document: ", error);
-      alert("Failed to add birthday. Please try again.");
+      console.error("Error adding/editing birthday: ", error);
+      alert("Failed to add/edit birthday. Please try again.");
+      setIsLoading(false);
     }
   };
 
@@ -45,7 +64,7 @@ const BirthdayForm = () => {
             <button
               type="button"
               className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
-              onClick={() => setFormModal(false)}
+              onClick={onClose}
             >
               <svg
                 className="w-5 h-5"
@@ -66,12 +85,12 @@ const BirthdayForm = () => {
             onSubmit={handleSubmit}
           >
             <h3 className="text-xl font-medium text-gray-900">
-              Add a new birthday
+              {initialData.id ? "Edit Birthday" : "Add a new birthday"}
             </h3>
             <div>
               <label
-                for="name"
-                className="block mb-2 text-sm font-medium text-gray-900 "
+                htmlFor="name"
+                className="block mb-2 text-sm font-medium text-gray-900"
               >
                 Name
               </label>
@@ -81,15 +100,15 @@ const BirthdayForm = () => {
                 onChange={(e) => setName(e.target.value)}
                 name="name"
                 id="name"
-                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                 placeholder="Tyler Durden"
-                required="true"
+                required
               />
             </div>
             <div>
               <label
-                for="date"
-                className="block mb-2 text-sm font-medium text-gray-900 "
+                htmlFor="date"
+                className="block mb-2 text-sm font-medium text-gray-900"
               >
                 Birth date
               </label>
@@ -99,31 +118,44 @@ const BirthdayForm = () => {
                 onChange={(e) => setDate(e.target.value)}
                 name="date"
                 id="date"
-                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
-                required="true"
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                required
               />
             </div>
             <div>
               <label
-                for="image"
-                className="block mb-2 text-sm font-medium text-gray-900 "
+                htmlFor="image"
+                className="block mb-2 text-sm font-medium text-gray-900"
               >
                 Image
               </label>
               <input
-                className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none "
+                className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none"
                 id="file_input"
                 onChange={(e) => setImageFile(e.target.files[0])}
                 name="image"
                 type="file"
               />
+              {imageUrl && !imageFile && (
+                <img
+                  src={imageUrl}
+                  alt="Birthday"
+                  className="w-20 h-20 mt-2 rounded-full"
+                />
+              )}
             </div>
             <button
               type="submit"
               disabled={isLoading}
               className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
             >
-              {isLoading ? "Adding birthday..." : "Add Birthday"}
+              {isLoading
+                ? initialData.id
+                  ? "Editing birthday..."
+                  : "Adding birthday..."
+                : initialData.id
+                ? "Edit Birthday"
+                : "Add Birthday"}
             </button>
           </form>
         </div>
