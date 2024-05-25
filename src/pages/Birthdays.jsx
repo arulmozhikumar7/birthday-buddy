553a46637c9323c683b6338fb27bcbcbd6c00bab
Arulmozhikumar <arulmozhikumar7@gmail.com>
2024-05-25 21:47:21 +0530

    feat: add Birthdays.jsx to display upcoming birthdays and table

---
+++ src/pages/Birthdays.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BirthdayTable from "../components/BirthdayTable";
+
+const Birthdays = () => {
+  return (
+    <>
+      <BirthdayTable />
+    </>
+  );
+};
+
+export default Birthdays;
