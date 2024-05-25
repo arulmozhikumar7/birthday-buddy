4320253c995a007600df95c16c37d527bdde2752
Arulmozhikumar <arulmozhikumar7@gmail.com>
2024-05-25 21:49:08 +0530

    feat: add Home.jsx with Calendar Component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Calendar from "../components/Calendar";
+
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <Calendar />
+    </>
+  );
+};
+
+export default Home;
