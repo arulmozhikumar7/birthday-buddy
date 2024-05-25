e4b544ab32d31e8e7eb7236f68c1b2f00a62da68
Arulmozhikumar <arulmozhikumar7@gmail.com>
2024-05-25 21:41:00 +0530

    refactor: add Link(Routes),Login,Logout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
+import { useUser } from "../contexts/store";
+import { auth, signInWithGoogle } from "../Auth/firebase";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { user } = useUser();
 
   return (
     <>
-      <nav className="bg-white border-gray-200">
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a
-            href=""
+      <nav className="bg-white border-gray-200 dark:bg-gray-900">
+        <div className="flex flex-wrap items-center justify-between max-w-screen-xl p-4 mx-auto">
+          <Link
+            to="/"
             className="flex items-center space-x-3 rtl:space-x-reverse"
           >
             <img
@@ -16,82 +19,74 @@ const Navbar = () => {
               className="h-8"
               alt="Birthday Buddy Logo"
             />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap">
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
               Birthday Buddy
             </span>
-          </a>
-          <button
-            onClick={() => setIsMenuOpen(!isMenuOpen)}
-            data-collapse-toggle="navbar-default"
-            type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
-            aria-controls="navbar-default"
-            aria-expanded={isMenuOpen ? "true" : "false"}
-          >
-            <span className="sr-only">Open main menu</span>
-            <svg
-              className="w-5 h-5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 17 14"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 1h15M1 7h15M1 13h15"
+          </Link>
+          <div className="flex flex-wrap items-center space-x-6 rtl:space-x-reverse">
+            {user && (
+              <button
+                onClick={() => auth.signOut()}
+                className="text-sm text-blue-600 dark:text-blue-500 hover:underline"
+              >
+                Logout
+              </button>
+            )}
+            {user ? (
+              <img
+                src={user?.photoURL}
+                className="order-2 w-8 h-8 rounded-full"
+                alt=""
               />
-            </svg>
-          </button>
-          <div
-            className={`w-full md:block md:w-auto ${
-              isMenuOpen ? "block" : "hidden"
-            }`}
-            id="navbar-default"
-          >
-            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white">
+            ) : (
+              <a
+                onClick={signInWithGoogle}
+                className="order-1 text-sm text-gray-500 dark:text-white hover:underline"
+              >
+                Login
+              </a>
+            )}
+          </div>
+        </div>
+      </nav>
+      <nav className="bg-gray-50 dark:bg-gray-700">
+        <div className="max-w-screen-xl px-4 py-3 mx-auto">
+          <div className="flex items-center">
+            <ul className="flex flex-row mt-0 space-x-8 text-sm font-medium rtl:space-x-reverse">
               <li>
-                <a
-                  href="#"
-                  className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0"
+                <Link
+                  to="/"
+                  className="text-gray-900 dark:text-white hover:underline"
                   aria-current="page"
                 >
                   Home
-                </a>
+                </Link>
               </li>
+              {user && (
+                <li>
+                  <Link
+                    to="/birthdays"
+                    className="text-gray-900 dark:text-white hover:underline"
+                  >
+                    Birthdays
+                  </Link>
+                </li>
+              )}
               <li>
-                <a
-                  href="#"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
+                <Link
+                  to="/about"
+                  className="text-gray-900 dark:text-white hover:underline"
                 >
                   About
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href="#"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
+                <Link
+                  to="/help"
+                  className="text-gray-900 dark:text-white hover:underline"
                 >
-                  Services
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
-                >
-                  Pricing
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
-                >
-                  Contact
-                </a>
+                  Help
+                </Link>
               </li>
             </ul>
           </div>
