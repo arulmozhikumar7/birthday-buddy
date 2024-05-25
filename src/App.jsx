84726045007500ecdb9a1c40497082dd1b102d72
Arulmozhikumar <arulmozhikumar7@gmail.com>
2024-05-25 21:51:10 +0530

    refactor: add firebase Authentication, BrowserRoutes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect } from "react";
+import { auth, onAuthStateChanged } from "./Auth/firebase";
+import { useUser } from "./contexts/store";
+import { getBirthdays } from "./hooks/useGetBirthdays";
+import Calendar from "./components/Calendar";
+import BirthdayTable from "./components/BirthdayTable";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import Navbar from "./components/Navbar";
+import InfoPage from "./pages/InfoPage";
+import About from "./pages/About";
+import Help from "./pages/Help";
+const App = () => {
+  const { user, setUser, setBirthdays } = useUser();
 
-function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user ? user : null);
+      if (user) {
+        getBirthdays(user ? user.uid : null)
+          .then((result) => {
+            setBirthdays(result);
+          })
+          .catch((error) => {
+            console.error("Error fetching birthdays: ", error);
+          });
+      }
+    });
+    return () => unsubscribe();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        {user ? (
+          <>
+            <Route path="/" element={<Calendar />} />
+          </>
+        ) : (
+          <Route path="/" element={<InfoPage />} />
+        )}
+        {user && <Route path="/birthdays" element={<BirthdayTable />} />}
+        <Route path="about" element={<About />} />
+        <Route path="help" element={<Help />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+      <Toaster position="top-center" duration={3000} />
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
